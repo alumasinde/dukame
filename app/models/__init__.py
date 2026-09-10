dae1d7b77d2be626aee4f1623d382c792c99b7b6
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-10 17:20:00 +0300

    clean model package exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    pass
-
-
+from app.models.base import Base
 from app.models.identity import Tenant, TenantUser, User
 
 __all__ = ["Base", "Tenant", "TenantUser", "User"]
