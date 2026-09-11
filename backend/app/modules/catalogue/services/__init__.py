0c25c22015a8608dbf72738d089cec95273bc31c
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 09:36:46 +0300

    feat(catalogue): add service exports

---
+++ backend/app/modules/catalogue/services/__init__.py
@@ -0,0 +1,5 @@
+from app.modules.catalogue.services.category import CategoryService
+from app.modules.catalogue.services.product import ProductService
+from app.modules.catalogue.services.store import StoreService
+
+__all__ = ["StoreService", "CategoryService", "ProductService"]
