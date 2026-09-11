9be130b1e8298b53a2228911d90923770fea462c
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 09:36:25 +0300

    feat(catalogue): add repository exports

---
+++ backend/app/modules/catalogue/repositories/__init__.py
@@ -0,0 +1,5 @@
+from app.modules.catalogue.repositories.category import CategoryRepository
+from app.modules.catalogue.repositories.product import ProductRepository
+from app.modules.catalogue.repositories.store import StoreRepository
+
+__all__ = ["StoreRepository", "CategoryRepository", "ProductRepository"]
