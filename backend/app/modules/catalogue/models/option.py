f9acecaaa956e8f02a613ee25c3184c238a92ad5
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 10:56:29 +0300

    fix product option type checking

--- backend/app/modules/catalogue/models/option.py
+++ backend/app/modules/catalogue/models/option.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 from sqlalchemy import BigInteger, DateTime, ForeignKey, Index, Integer, String, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+
+if TYPE_CHECKING:
+    from app.modules.catalogue.models.option_value import ProductOptionValue
 
 
 class ProductOption(Base):
