83129ae2f4151f79b3f913f9c90668e0aebc1c11
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 09:35:35 +0300

    feat(catalogue): add catalogue model exports

---
+++ backend/app/modules/catalogue/models/__init__.py
@@ -0,0 +1,5 @@
+from app.modules.catalogue.models.category import Category
+from app.modules.catalogue.models.product import Product
+from app.modules.catalogue.models.store import Store
+
+__all__ = ["Store", "Category", "Product"]
