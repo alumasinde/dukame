df805ec1564249d5f185159971013fa43074a01c
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 14:33:30 +0300

    feat: expose business type model

---
+++ backend/app/modules/tenancy/models/__init__.py
@@ -0,0 +1,4 @@
+from app.modules.tenancy.models.business_type import BusinessType
+from app.modules.tenancy.models.tenant import Tenant, TenantUser
+
+__all__ = ["BusinessType", "Tenant", "TenantUser"]
