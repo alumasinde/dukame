f64fd4ab138db29ad12603008a93e3a3ad910528
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 09:50:09 +0300

    style(catalogue): load catalogue styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './styles.css'
+import './styles/catalogue.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
