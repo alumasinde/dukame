eed01582d38fbd2839bdcd0a5989465ec2a2fcd6
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-11 09:44:42 +0300

    feat(catalogue): register option and variant models

--- backend/app/modules/catalogue/models/__init__.py
+++ backend/app/modules/catalogue/models/__init__.py
@@ -1,5 +1,17 @@
 from app.modules.catalogue.models.category import Category
+from app.modules.catalogue.models.option import ProductOption
+from app.modules.catalogue.models.option_value import ProductOptionValue
 from app.modules.catalogue.models.product import Product
 from app.modules.catalogue.models.store import Store
+from app.modules.catalogue.models.variant import ProductVariant
+from app.modules.catalogue.models.variant_option_value import ProductVariantOptionValue
 
-__all__ = ["Store", "Category", "Product"]
+__all__ = [
+    "Store",
+    "Category",
+    "Product",
+    "ProductOption",
+    "ProductOptionValue",
+    "ProductVariant",
+    "ProductVariantOptionValue",
+]
